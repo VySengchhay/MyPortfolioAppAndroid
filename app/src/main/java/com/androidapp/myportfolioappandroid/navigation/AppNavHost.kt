f4de0c539c8f7473c61ed614ef6ef26a672a938a
vysengchhay <sengchhaygg@gmail.com>
2026-08-20 14:49:17 +0700

    Restructure navigation

--- app/src/main/java/com/androidapp/myportfolioappandroid/navigation/AppNavHost.kt
+++ app/src/main/java/com/androidapp/myportfolioappandroid/navigation/AppNavHost.kt
@@ -11,13 +11,19 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.androidapp.myportfolioappandroid.feature.apifeature.presentation.ApiScreen
 import com.androidapp.myportfolioappandroid.feature.auth.AuthState
 import com.androidapp.myportfolioappandroid.feature.auth.AuthViewModel
 import com.androidapp.myportfolioappandroid.feature.auth.login.LoginScreen
 import com.androidapp.myportfolioappandroid.feature.auth.signup.SignUpScreen
-import com.androidapp.myportfolioappandroid.feature.dashboard.navigation.DashboardRoute
-import com.androidapp.myportfolioappandroid.feature.dashboard.navigation.dashBoardGraph
+import com.androidapp.myportfolioappandroid.feature.componentfeature.presentation.ComponentScreen
+import com.androidapp.myportfolioappandroid.feature.dashboard.presentation.DashBoardScreen
+import com.androidapp.myportfolioappandroid.feature.layoutfeature.presentation.LayoutScreen
+import com.androidapp.myportfolioappandroid.feature.layoutfeature.presentation.lazycolumn.ColumnLayoutScreen
+import com.androidapp.myportfolioappandroid.feature.layoutfeature.presentation.lazyrow.RowLayoutScreen
 import com.androidapp.myportfolioappandroid.feature.profile.presentation.ProfileScreen
+import com.androidapp.myportfolioappandroid.feature.sytemanddevice.presentation.SystemAndDeviceScreen
 
 @Composable
 fun AppNavHost(
@@ -27,11 +33,18 @@ fun AppNavHost(
     val authUiState by authViewModel.authStateFlow.collectAsStateWithLifecycle()
     val context = LocalContext.current
 
+
     LaunchedEffect(authUiState) {
         when (authUiState) {
-            is AuthState.Authenticated -> navController.navigate(
-                DashboardRoute
-            )
+            is AuthState.Authenticated -> {
+                navController.navigate(DashboardRoute) {
+                    popUpTo(LoginRoute) {
+                        inclusive = true
+                    }
+                    launchSingleTop = true
+                }
+            }
+
             is AuthState.Error -> Toast.makeText(
                 context,
                 (authUiState as AuthState.Error).message,
@@ -67,28 +80,127 @@ fun AppNavHost(
             )
         }
 
-        dashBoardGraph(
-            navController = navController
-        )
-
         composable<ProfileRoute> {
+            LaunchedEffect(authUiState) {
+                when (authUiState) {
+                    is AuthState.UnAuthenticated -> {
+                        navController.navigate(LoginRoute) {
+                            popUpTo(DashboardRoute) {
+                                inclusive = true
+                            }
+                            launchSingleTop = true
+                        }
+                    }
 
+                    else -> Unit
+                }
+            }
             ProfileScreen(
                 onBackClick = {
                     navController.popBackStack()
                 },
+                onEvent = authViewModel::onEvent
+            )
+        }
 
-                onLogoutClick = {
-                    navController.navigate(LoginRoute) {
-                        popUpTo(LoginRoute) {
-                            inclusive = true
-                        }
-                    }
+        composable<DashboardRoute> {
+            val userName by authViewModel.userName.collectAsStateWithLifecycle()
+
+            DashBoardScreen(
+                modifier = Modifier,
+                userName = userName,
+                onProfileClick = {
+                    navController.navigate(ProfileRoute)
                 },
-                authViewModel = authViewModel,
-                navController = navController
+                onCategoryClick = { route ->
+                    when (route) {
+                        "layout" -> navController.navigate(
+                            LayoutRoute(route = route)
+                        )
+
+                        "component" -> navController.navigate(
+                            ComponentRoute(route = route)
+                        )
+
+                        "api" -> navController.navigate(
+                            ApiRoute(route = route)
+                        )
+
+                        "system_device" -> navController.navigate(
+                            DeviceSystemRoute(route = route)
+                        )
+
+                        else -> Unit
+                    }
+                }
+            )
+        }
+
+        composable<RowLayoutRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<RowLayoutRoute>()
+
+            RowLayoutScreen(
+                layoutId = route.layoutId,
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable<ColumnLayoutRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<ColumnLayoutRoute>()
+
+            ColumnLayoutScreen(
+                layoutId = route.layoutId,
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable<LayoutRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<LayoutRoute>()
+            LayoutScreen(
+                modifier = Modifier,
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFeatureClick = {
+
+                }
+            )
+        }
+
+        composable<ComponentRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<ComponentRoute>()
+            ComponentScreen(
+                modifier = Modifier,
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFeatureClick = {}
+            )
+        }
+
+        composable<ApiRoute> { backStackEntry ->
+            val route = backStackEntry.toRoute<ApiRoute>()
+            ApiScreen(
+                modifier = Modifier,
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFeatureClick = {}
+            )
+        }
+
+        composable<DeviceSystemRoute> { backStackEntry ->
+            SystemAndDeviceScreen(
+                modifier = Modifier,
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFeatureClick = {}
             )
         }
     }
-
 }
--- app/src/main/java/com/androidapp/myportfolioappandroid/navigation/AppRoute.kt
+++ app/src/main/java/com/androidapp/myportfolioappandroid/navigation/AppRoute.kt
@@ -10,3 +10,37 @@ data object SignUpRoute
 
 @Serializable
 data object ProfileRoute
+
+@Serializable
+data object DashboardRoute
+
+@Serializable
+data class LayoutRoute(
+    val route : String
+)
+
+@Serializable
+data class ComponentRoute(
+    val route : String
+)
+
+@Serializable
+data class ApiRoute(
+    val route : String
+)
+
+@Serializable
+data class DeviceSystemRoute(
+    val route : String
+)
+
+
+@Serializable
+data class RowLayoutRoute(
+    val layoutId: String
+)
+
+@Serializable
+data class ColumnLayoutRoute(
+    val layoutId: String
+)
